849ce17368bd2b8be9d41261e0c324f8119b6125
t-miyazaki <tsuba.miya2345@gmail.com>
2025-10-12 18:56:12 +0900

    FAQを追加しました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
-"use client";
+'use client'
 
-import React from "react";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import Service from "../components/Service";
-import About from "../components/About";
-import Cast from "../components/Cast";
-import Function from "../components/Function";
-import Voice from "../components/Voice";
-import Flow from "../components/Flow";
-import CTA from "../components/CTA";
-import Footer from "../components/Footer";
+import React from 'react'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+import Service from '../components/Service'
+import About from '../components/About'
+import Cast from '../components/Cast'
+import Function from '../components/Function'
+import Voice from '../components/Voice'
+import Flow from '../components/Flow'
+import Faq from '../components/Faq'
+import CTA from '../components/CTA'
+import Footer from '../components/Footer'
 
 export default function HomePage() {
   return (
@@ -24,7 +25,9 @@ export default function HomePage() {
       <CTA />
       <Voice />
       <Flow />
+      <Faq />
+      <CTA />
       <Footer />
     </main>
-  );
+  )
 }
